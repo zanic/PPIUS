e10878b722cc2d76578197eb2704c78947cdddaf
Zvonimir Anic <thecrnac@gmail.com>
2017-12-03 22:12:11 +0100

    Finished lab1

--- LAB_Serial/main.c
+++ LAB_Serial/main.c
@@ -1,8 +1,25 @@
+/* main.c */
+#include "main.h"
+
 int main(void)
 {
-	int i=0;
+	char c;
+	char exit_str[] = "now exiting loopback mode";
+	USART1_Init();
 	while(1)
 	{
-		i = i + 1;
+		if (USART1_Dequeue(&c) != 0)
+			if (c == 'x')
+			{
+				USART1_SendChar('\n');
+				for (uint8_t i=0; i < sizeof(exit_str)/sizeof(char); i++)
+				{
+					USART1_SendChar(exit_str[i]);
+				}
+				USART1_SendChar('\n');
+				break;
+			}
+				
+			USART1_SendChar(c);
 	}
 }
